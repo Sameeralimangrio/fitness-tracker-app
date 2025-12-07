b15cba9d8debc0f0cab72f77505d39c0311a87af
Sameeralimangrio <sameealimangrio@gmail.com>
2025-12-07 22:15:09 +0500

    exercises.js

---
+++ data/exercises.js
@@ -0,0 +1,15 @@
+
+export const exercises = [
+  {
+    id: '1',
+    name: 'Push Up',
+    image: 'https://example.com/pushup.png',
+    description: 'Push ups are great for upper body strength.'
+  },
+  {
+    id: '2',
+    name: 'Squat',
+    image: 'https://example.com/squat.png',
+    description: 'Squats help strengthen your legs and core.'
+  },
+];
